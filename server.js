3aa040969211785b606685ab042c1a3294e0a133
Yaekirua <111132084+Yaekirua@users.noreply.github.com>
2025-01-25 00:35:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -96,6 +96,6 @@ app.post('/delete/:id', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
